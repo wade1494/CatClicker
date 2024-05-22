427ba8da124c3f1cb449dbbe5a8a373a0f6188c7
Triet Le <letri0077@ccsdschools.com>
2024-05-21 22:16:39 -0400

    Fix replit stupid bugs

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-    deps = [
-      pkgs.run
-        pkgs.graalvm17-ce
-        pkgs.maven
-        pkgs.replitPackages.jdt-language-server
-        pkgs.replitPackages.java-debug
-    ];
-}
